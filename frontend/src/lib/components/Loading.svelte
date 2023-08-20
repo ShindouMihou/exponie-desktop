b8a3407bdff78c4b22904daa9b88831f27e4124d
Shindou Mihou <git@mihou.pw>
2023-08-21 04:14:34 +0800

    feat: make loading screen draggable
    Signed-off-by: Shindou Mihou <git@mihou.pw>

--- frontend/src/lib/components/Loading.svelte
+++ frontend/src/lib/components/Loading.svelte
@@ -1,4 +1,4 @@
-<div class="w-full m-auto">
+<div class="w-full m-auto" style="--wails-draggable:drag">
     <div class="flex w-full items-center justify-center m-auto animate-pulse">
         <p class="font-bold text-2xl uppercase text-black bg-white p-1 animate-bounce select-none">EX</p>
     </div>
